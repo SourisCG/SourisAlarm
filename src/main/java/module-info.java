0ef3bc2f52716da5908593f5dc911881cedb5112
Sebastián García <sebastian.garciab2004@gmail.com>
2026-02-08 22:14:54 -0600

    Arreglo de empaquetado

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.souris {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-    requires java.desktop;
-
-    opens com.souris to javafx.fxml;
-    exports com.souris;
-}
